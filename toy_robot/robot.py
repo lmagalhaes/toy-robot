05e45c5e6376ac4d2dd92639d9ac875ecee29044
Leandro Magalhaes <leandro.magalhaes@theiconic.com.au>
2020-09-17 15:35:05 +1000

    Reduce complexity on movee method

--- toy_robot/robot.py
+++ toy_robot/robot.py
@@ -56,17 +56,16 @@ class Robot:
         if not self.location:
             return
 
-        if self.direction == CardinalCoordinates.NORTH.name:
-            new_location = self.location.sum(Point(0, 1))
+        increase_coordinate = {
+            CardinalCoordinates.NORTH.name: Point(0, 1),
+            CardinalCoordinates.SOUTH.name: Point(0, -1),
+            CardinalCoordinates.EAST.name: Point(1, 0),
+            CardinalCoordinates.WEST.name: Point(-1, 0)
+        }
 
-        if self.direction == CardinalCoordinates.SOUTH.name:
-            new_location = self.location.sum(Point(0, -1))
-
-        if self.direction == CardinalCoordinates.EAST.name:
-            new_location = self.location.sum(Point(1, 0))
-
-        if self.direction == CardinalCoordinates.WEST.name:
-            new_location = self.location.sum(Point(-1, 0))
+        new_location = self.location.sum(
+            increase_coordinate.get(self.direction)
+        )
 
         if self.is_within_boundaries(new_location):
             self.location = new_location
